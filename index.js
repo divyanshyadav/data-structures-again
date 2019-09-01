6d027508fd492cf9e1b1df8cfbbef99a3a687366
DY <dy@dAir.local>
2019-09-01 18:44:38 +0530

    add webpack

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-const { BST } = require('./src/tree')
-const Stack = require('./src/stack')
-const Queue = require('./src/queue')
-const Heap = require('./src/heap')
-
-module.exports = {
-    BST,
-    Stack,
-    Queue,
-    Heap
-}
+const ds = require('./src')
+module.exports = ds
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const { BST } = require('./tree')
+const Stack = require('./stack')
+const Queue = require('./queue')
+const Heap = require('./heap')
+
+module.exports = {
+    BST,
+    Stack,
+    Queue,
+    Heap
+}
