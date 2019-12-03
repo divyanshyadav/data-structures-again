6c696b0436a95e32e44c0d935b8978e0f975554c
Divyansh <divyansh.yadav@wipro.com>
2019-12-03 12:56:06 +0530

    add disjoint-set ds to lib

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const Stack = require('./src/stack')
 const Queue = require('./src/queue')
 const Heap = require('./src/heap')
 const Graph = require('./src/graph')
+const DisjointSet = require('./src/disjoint-set')
 
 module.exports = {
     BST,
     Stack,
     Queue,
     Heap,
-    Graph
+    Graph,
+    DisjointSet
 }
